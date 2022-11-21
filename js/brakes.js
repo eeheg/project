f547bdea655a8a19e1d48138a262639e7bf9fad3
eeheg <sjh8924@naver.com>
2022-11-21 16:21:42 +0900

    continue

---
+++ js/brakes.js
@@ -0,0 +1,54 @@
+const bycHand=document.getElementsByClassName("byc_hand")[0]
+bycHand.addEventListener("click", () => {
+    document.querySelector("body").style.backgroundPosition = "-1186px";
+    
+    document.querySelector(".content").style.animation = "fadeout 1s";
+    setTimeout(() => {
+        document.querySelector(".content").style.opacity = "0";
+    }, 900);
+    document.querySelector(".buyone").style.transform = "translate(0px, 200px)";
+})
+
+const bycChair=document.getElementsByClassName("byc_chair")[0]
+bycChair.addEventListener("click", () => {
+    document.querySelector("body").style.backgroundPosition = "-1186px";
+    
+    document.querySelector(".content").style.animation = "fadeout 1s";
+    setTimeout(() => {
+        document.querySelector(".content").style.opacity = "0";
+    }, 900);
+    document.querySelector(".buyone").style.transform = "translate(0px, 200px)";
+})
+
+const bycCenter=document.getElementsByClassName("byc_center")[0]
+bycCenter.addEventListener("click", () => {
+    document.querySelector("body").style.backgroundPosition = "-1186px";
+    
+    document.querySelector(".content").style.animation = "fadeout 1s";
+    setTimeout(() => {
+        document.querySelector(".content").style.opacity = "0";
+    }, 900);
+    document.querySelector(".buyone").style.transform = "translate(0px, 200px)";
+})
+
+const bycMotor=document.getElementsByClassName("byc_motor")[0]
+bycMotor.addEventListener("click", () => {
+    document.querySelector("body").style.backgroundPosition = "-1186px";
+    
+    document.querySelector(".content").style.animation = "fadeout 1s";
+    setTimeout(() => {
+        document.querySelector(".content").style.opacity = "0";
+    }, 900);
+    document.querySelector(".buyone").style.transform = "translate(0px, 200px)";
+})
+
+const bycWheel=document.getElementsByClassName("byc_wheel")[0]
+bycWheel.addEventListener("click", () => {
+    document.querySelector("body").style.backgroundPosition = "-1186px";
+    
+    document.querySelector(".content").style.animation = "fadeout 1s";
+    setTimeout(() => {
+        document.querySelector(".content").style.opacity = "0";
+    }, 900);
+    document.querySelector(".buyone").style.transform = "translate(0px, 200px)";
+})
